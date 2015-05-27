a916ea0d36f2e895768dc0eb9b3a752631de07c3
hirosh7 <hirosh7@gmail.com>
2015-05-27 00:40:39 -0600

    refactored building method as a callable package method

--- com/frostycold/apps/ampas.py
+++ com/frostycold/apps/ampas.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 import unicodedata
 
-if __name__ == '__main__':
 
-    s_actor_file = 'data/AMPAS_1927_2014_s_actor.html'
-    s_actress_file = 'data/AMPAS_1927_2014_s_actress.html'
+def build_actor_list(data_files):
 
-    data_files = [s_actor_file, s_actress_file]
     empty_dict = {}
     actor_entry = {}
     actor_list = []
@@ -29,16 +26,13 @@ if __name__ == '__main__':
                     if a['class'][0] == 'awardYearHeader':
                         date_str = a.contents[0]
                         award_date = date_str.split(' ')[0]
-                        print award_date
                 # Get actor and movie
                 href_str = a['href']
                 target_str = unicodedata.normalize('NFD', a.contents[0]).encode('ascii', 'ignore')
                 if href_str.find('BSNominationID=') != -1:
                     actor = target_str
-                    print 'Actor: {0} / '.format(actor),
                 elif href_str.find('BSFilmID=') != -1:
                     movie = target_str
-                    print 'Movie: {0}'.format(movie)
                     target_found = True
 
             if target_found:
@@ -53,4 +47,4 @@ if __name__ == '__main__':
                 actor_entry = empty_dict.copy()
                 target_found = False
 
-    print actor_list
+    return actor_list
